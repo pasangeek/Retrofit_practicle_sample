70979ed88f7b20511aa92016bb6ec4d26bd84b7a
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-03-12 01:22:28 +0900

    data class added

--- app/src/main/java/com/example/retrofit_practicle/UserResponse.kt
+++ app/src/main/java/com/example/retrofit_practicle/UserResponse.kt
@@ -1,4 +1,28 @@
 package com.example.retrofit_practicle
 
-class UserResponse {
-}
+class UserResponse (
+
+    var page: Int,
+    var perPage: Int,
+    var total: Int,
+    var totalPages,
+    var data: List<user>,
+    var support:Support
+)
+
+data class user (
+
+    var id: Int,
+    var email: String,
+    var firstname: String,
+    var lastName: String,
+    var avatar: String
+
+
+)
+data class Support(
+
+    var url :String,
+    var text:String
+
+)
